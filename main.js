d12923e2486d3f8e188a4b54863dac5d1f1492e4
ygm2013 <ygangire@gmail.com>
2023-02-14 10:28:17 +0200

    linear gradient

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 /* 11 Feb 2023 YGangire */
 /* routine to get photos on the file system and display them in the gallery*/
 
+/** this is the outer into which the div containing the gallert photos will be inserted */
 const galleryDiv = document.getElementById("galleryId")
 
+
 for (let i=1; i< 10; i++){
-    const imgDiv =  document.createElement("div");
-    const imgElem = document.createElement("img");
+    const imgDiv =  document.createElement("div"); /* div to hold the images each image is soorounded by its own div*/
+    const imgElem = document.createElement("img"); /* this is img element and all, properties of the images are set on this*/
 
-    imgElem.src =  `img/${i}.jpg`;
+    imgElem.src =  `img/${i}.jpg`;  /* URL of each individual element */
 
-    imgDiv.appendChild(imgElem);
+    imgDiv.appendChild(imgElem); /* add the image to the div */
 
-    galleryDiv.appendChild(imgDiv);
+    galleryDiv.appendChild(imgDiv); /* add the div with the image to the main div i.e. "galleryId" */
 
-    imgDiv.classList.add("image");
+    imgDiv.classList.add("image"); /* add the image for styline the gallery image */
 }
-// console.log(imgDiv);
-console.log(galleryDiv);
